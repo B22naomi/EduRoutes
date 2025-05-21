7c28bb072bd7377beb780d2e471fe4abe4e0973c
Naomi Bitutu <naomibitutu@gmail.com>
2025-05-21 22:26:47 +0300

    Initial commit of EduRoutes website

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type React from "react"
+import "@/app/globals.css"
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "EduRoutes - Revolutionize Your School Bus Routes",
+  description:
+    "EduRoutes uses advanced algorithms and real-time tracking to optimize school bus routes, ensuring safer journeys, reduced costs, and peace of mind for schools and parents.",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
